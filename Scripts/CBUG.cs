1cd1c894ee5a6e7e282502ce14f14cc16b292d0d
Eliot Carney-Seim <ecsit39@gmail.com>
2017-05-21 19:52:44 -0400

    NOTICE: PLEASE KEEP THIS CHANGELOG CLEAN. CONSIDER THIS A README. Organize in more recent as higher.
    v0.01 - 05/21/2017 - First Commit. CBUG is a replacement for Debug.Log that allows for seeing debug logs on-screen post-editor. Tools is a script for simple and common functions. Please
    feel free to update it, but make your own fork first. Anything I find myself using more than once will likely be added to this, but anything added to this MUST be cleaned up and commented.

---
+++ Scripts/CBUG.cs
@@ -0,0 +1,232 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+/// <summary>
+/// A statically available debugger for on-screen data visualization.
+/// Focus on ease-of-use and not optimized.
+///  - Eliot Carney-Seim
+/// </summary>
+///
+[RequireComponent (typeof(Text))]
+public class CBUG : MonoBehaviour {
+
+    #region Public Unity-Assigned Vars
+    public bool ALL_DEBUG_TOGGLE;
+    public bool DebugOnOfficial;
+    public bool SendToConsole;
+    public bool DisableOnScreen;
+    public float ClearTime;
+    public bool Clear;
+    public int ClearAmount;
+    public bool ClearAll;
+    #endregion
+
+    #region Private Vars
+    private Text logText;
+    private LinkedList<string> lines;
+    private LinkedList<int> occurrences;
+    private LinkedListNode<string> tempLinesIter;
+    private LinkedListNode<int> tempOccurIter;
+    private bool isParented;
+    private float previousClear;
+    private bool neverClear;
+    private int maxLines = 33; //Tested, based on 24pt Min.
+    #endregion
+
+
+    // Use this for initialization
+    void Awake () {
+        logText = GetComponent<Text>();
+        lines = new LinkedList<string>();
+        occurrences = new LinkedList<int>();
+        if (DisableOnScreen)
+            logText.color = new Color(0, 0, 0, 0);
+        if (ClearTime == 0)
+            neverClear = true;
+        if (ClearAll)
+            ClearAmount = -1;
+
+        transform.tag = "CBUG";
+        previousClear = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!ALL_DEBUG_TOGGLE)
+            return;
+
+        if (!Debug.isDebugBuild && !DebugOnOfficial) {
+            ALL_DEBUG_TOGGLE = false;
+            Do("Official Build, Disabled!");
+            return;
+        }
+
+        if (Clear) {
+            Clear = false;
+            _ClearLines(ClearAmount);
+        }
+
+        if (!isParented && GameObject.Find("CanvasGroup") != null) {
+            isParented = true;
+            GameObject.Find("CanvasGroup").transform.SetParent(transform, true);
+        }
+
+        logText.text = "";
+        tempLinesIter = lines.First;
+        tempOccurIter = occurrences.First;
+        for(int x = 0; x < lines.Count; x++) {
+            logText.text += tempLinesIter.Value + " || " + tempOccurIter.Value + "\n";
+            tempLinesIter = tempLinesIter.Next;
+            tempOccurIter = tempOccurIter.Next;
+        }
+
+        if (lines.Count > maxLines) {
+            for (int x = 0; x < lines.Count - maxLines; x++) {
+                lines.RemoveFirst();
+                occurrences.RemoveFirst();
+            }
+        }
+
+        if(!neverClear && Time.time - previousClear > ClearTime) {
+            Clear = true;
+            previousClear = Time.time;
+        }
+	}
+
+    #region Debug Aliases
+    public static void Log(string line)
+    {
+        GetRef()._Print(line);
+    }
+
+    public static void Do(string line)
+    {
+        GetRef()._Print(line);
+    }
+
+    public static void Log(string line, bool debugOn)
+    {
+        GetRef()._Print(line, debugOn);
+    }
+
+    public static void Do(string line, bool debugOn)
+    {
+        GetRef()._Print(line, debugOn);
+    }
+    #endregion
+
+
+    #region Helper Functions
+    private void _ClearLines(int amount)
+    {
+        if (lines.Count == 0)
+            return;
+
+        if(amount == -1) {
+            lines.Clear();
+            occurrences.Clear();
+        }
+        else 
+        {
+            amount = amount > lines.Count ? lines.Count : amount;
+            for(int x = 0; x < amount; x++) {
+                lines.RemoveFirst();
+                occurrences.RemoveFirst();
+            }
+        }
+    }
+
+    private static CBUG GetRef()
+    {
+        return GameObject.FindGameObjectWithTag("CBUG").GetComponent<CBUG>();
+    }
+
+    private void _Print(string line)
+    {
+        if (!ALL_DEBUG_TOGGLE)
+            return;
+
+        if(SendToConsole)
+            Debug.Log(line);
+
+        if (lines.Find(line) != null) {
+            tempLinesIter = lines.First;
+            tempOccurIter = occurrences.First;
+            for (int x = 0; x < GetRef().lines.Count; x++) {
+                if (tempLinesIter.Value == line) {
+                    tempOccurIter.Value++;
+                    break;
+                }
+                tempLinesIter = tempLinesIter.Next;
+                tempOccurIter = tempOccurIter.Next;
+            }
+        } else {
+            lines.AddLast(line);
+            occurrences.AddLast(1);
+        }
+    }
+
+    private void _Print(string line, bool debugOn)
+    {
+        if (ALL_DEBUG_TOGGLE && debugOn) {
+            if (line == null)
+                _Print("Null @ " + System.Environment.StackTrace);
+            else
+                _Print(line);
+        }
+    }
+
+    private void _Error(string line)
+    {
+        _Print("ERROR <~> " + line);
+        Debug.Log("ERROR <~> " + line);
+        logText.color = Color.red;
+    }
+
+    private void _SrsError(string line)
+    {
+        _Error(line);
+        throw new System.Exception("ERROR <~> " + line);
+    }
+    #endregion
+
+    #region Public Static Functions
+    /// <summary>
+    /// -1 for All lines.
+    /// </summary>
+    /// <param name="amount"></param>
+    public static void ClearLines(int amount)
+    {
+        GetRef()._ClearLines(amount);
+    }
+
+    public static void Print(string line)
+    {
+        GetRef()._Print(line);
+    }
+
+    public static void Print(string line, bool debugOn)
+    {
+        GetRef()._Print(line, debugOn);
+    }
+
+    public static void Error(string line)
+    {
+        GetRef()._Error(line);
+    }
+
+    public static void SrsError(string line)
+    {
+        GetRef()._SrsError(line);
+    }
+
+    public static bool DEBUG_ON
+    {
+        get {
+            return GetRef().ALL_DEBUG_TOGGLE;
+        }
+    }
+    #endregion
+}
---
+++ Scripts/Tools.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A set of easily callable functions built to be used through a singleton in Unity.
+/// Contributors:
+///  - Eliot Leo Carney-Seim
+///  
+/// Further Reading:
+/// http://wiki.unity3d.com/index.php/Singleton
+/// http://wiki.unity3d.com/index.php/Toolbox
+/// </summary>
+public class Tools : MonoBehaviour {
+
+    public delegate void VanillaFunction();
+
+    #region Singleton Control
+    private int priority;
+    #endregion
+
+    protected Tools ()
+    {
+        //No instantiation!
+    }
+
+    // Use this for initialization
+    void Start () {
+        tag = "Tools";
+        ensureSingleton();
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    #region Public Static Hooks
+    /// <summary>
+    /// For delaying a function call by a single frame.
+    /// </summary>
+    /// <param name="Call">Parameter-less function</param>
+    /// <param name="Time">How long to delay by. To delay by 1 frame, Time == 0.</param>
+    /// <returns></returns>
+    public static void DelayFunction(VanillaFunction Call, float Time)
+    {
+        GetSelf()._delayFunction(Call, Time);
+    }
+
+    /// <summary>
+    /// Returns Tools class. GetComponent<Tools>() supplies the same functionality.
+    /// </summary>
+    /// <returns></returns>
+    public static Tools GetSelf()
+    {
+        return GameObject.FindGameObjectWithTag("Tools").GetComponent<Tools>()._getSelf();
+    }
+
+    /// <summary>
+    /// For delaying an animation call by a single frame.
+    /// </summary>
+    /// <param name="Call">Animation object</param>
+    /// <param name="Time">How long to delay by. To delay by 1 frame, Time == 0.</param>
+    /// <returns></returns>
+    public static void DelayAnim(Animator anim, float time, string clipName,
+        string trigger = "",
+        float floatParam = -1f,
+        int intParam = -1,
+        bool boolParam = false,
+        bool hasBoolParam = false)
+    {
+        GetSelf()._delayAnim(anim, time, clipName,
+            trigger: trigger,
+            floatParam: floatParam,
+            intParam: intParam,
+            boolParam: boolParam,
+            hasBoolParam: hasBoolParam);
+    }
+    #endregion
+
+
+    #region Private Core Functionality 
+    private IEnumerator __delayAnim(Animator anim, float time, string clipName,
+        string trigger = "",
+        float floatParam = -1f,
+        int intParam = -1,
+        bool boolParam = false,
+        bool hasBoolParam = false)
+    {
+        if (time <= 0f)
+            yield return 0f;
+        else
+            yield return new WaitForSeconds(time);
+
+        if (hasBoolParam)
+            anim.SetBool(clipName, boolParam);
+        else if (trigger != "")
+            anim.SetTrigger(trigger);
+        else if (floatParam != -1f)
+            anim.SetFloat(clipName, floatParam);
+        else if (intParam != -1)
+            anim.SetFloat(clipName, intParam);
+
+    }
+    private IEnumerator __DelayFunction(VanillaFunction call, float time)
+    {
+        if (time <= 0f)
+            yield return 0f;
+        else
+            yield return new WaitForSeconds(time);
+        call();
+    }
+
+    private Tools _getSelf()
+    {
+        return this;
+    }
+    #endregion
+
+    #region Helper Functions
+    private void _delayAnim(Animator anim, float time, string clipName,
+        string trigger = "",
+        float floatParam = -1f,
+        int intParam = -1,
+        bool boolParam = false,
+        bool hasBoolParam = false)
+    {
+        StartCoroutine(__delayAnim(anim, time, clipName, 
+            trigger: trigger,
+            floatParam: floatParam,
+            intParam  : intParam,
+            boolParam : boolParam,
+            hasBoolParam : hasBoolParam));
+    }
+    private void _delayFunction(VanillaFunction call, float time)
+    {
+        StartCoroutine(__DelayFunction(call, time));
+    }
+    private void ensureSingleton()
+    {
+        //Give all clones a chance to instantiate themselves.
+        _delayFunction(killSelfIfOthersExit, 0f);
+    }
+
+    private void killSelfIfOthersExit()
+    {
+        //All clones will get the list in the same order, applying th
+        if (GameObject.FindObjectsOfType(typeof(Tools)).Length > 1) { 
+            for (int x = 0; x < GameObject.FindObjectsOfType(typeof(Tools)).Length; x++)
+            {
+                (GameObject.FindObjectsOfType(typeof(Tools))[x] as Tools).Priority = x;
+            }
+            _delayFunction(killSelf, 0f);
+        }
+    }
+
+    private void killSelf()
+    {
+        if (priority != 0)
+            Destroy(gameObject);
+    }
+    #endregion
+
+    #region Getters and Setters
+    public int Priority {
+        set {
+            priority = value;
+        }
+    }
+    #endregion
+}
